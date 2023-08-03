f10cd72ec64452751b1eb431b3507b4697f9c791
KimberlyPeters <kimberlypeters468@gmail.com>
2023-08-03 11:18:23 -0700

    Height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,15 +7,14 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+
 	size_t left_height = 0, right_height = 0;
 
-	if (tree == NULL)
+	if (tree)
 	{
-		return (0);
+		left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((left_height > right_height) ? left_height : right_height);
 	}
-
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	return (1 + (left_height > right_height ? left_height : right_height));
+	return (0);
 }
